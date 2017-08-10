22fdbd23e503ecf2f4d9f806d7ec16f2e08372ab
Chandler Lagarde <chandler.lagarde@ibm.com>
2017-08-10 16:38:28 -0500

    Not Utilized!

--- strutsTemplate/src/main/java/com/ibm/actions/EditUserAction.java
+++ strutsTemplate/src/main/java/com/ibm/actions/EditUserAction.java
@@ -1,118 +1,118 @@
-package com.ibm.actions;
-
-import java.util.List;
-import com.opensymphony.xwork2.ActionSupport;
-
-import com.ibm.bo.*;
-import com.ibm.beans.*;
-
-public class EditUserAction extends ActionSupport {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7641976286350960735L;
-	
-	/*array list holds info*/
-	private List<Employee> employees = null;
-	private List<Account> employeeAccount = null;
-	
-	/*declartions*/
-	private String username;
-	private String password;
-	
-	private String firstName;
-	private String lastName;
-	
-
-	
-	private String email;
-	
-	public static final int MIN_PASSWORD_LENGTH = 8;
-	public static final int MAX_PASSWORD_LENGTH = 35;
-	
-
-	
-	/*add and edit employee*/
-	public String editUser()
-	{
-		System.out.println("hello");
-		employees = EmpBO.fetchEmployees();
-		
-				for(int k = 0; k<employees.size(); k++)
-				{
-					System.out.println("checking for id");
-					System.out.println(employees.get(k).getFirstName()+" "+ employees.get(k).getLastName() + "  "+ employees.get(k).getId());
-				}
-				
-		
-		return "SUCCESS";
-	}
-	
-	
-	
-	/*getter and setter*/
-	public String getUsername() {
-		return username;
-	}
-	
-	public String getPassword()
-	{
-		return password;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public void setPassword(String password)
-	{
-		this.password = password;
-		
-	}
-	
-	public List<Employee> getEmployees()
-	{
-		return employees;
-	}
-	
-	public void setEmployees(List<Employee> employees)
-	{
-		this.employees = employees;
-	}
-	
-	
-	public String getFirstname()
-	{
-		return firstName;
-	}
-	
-	public void setFirstname(String firstName) {
-		this.firstName = firstName;
-	
-	}
-	
-	public String getLastname()
-	{
-		return lastName;
-	}
-	
-	public void setLastname(String lastName) {
-		this.lastName = lastName;
-	
-	}
-	
-	public String getEmail()
-	{
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	
-	}
-	
-	
-	
-	
-}
+//package com.ibm.actions;
+//
+//import java.util.List;
+//import com.opensymphony.xwork2.ActionSupport;
+//
+//import com.ibm.bo.*;
+//import com.ibm.beans.*;
+//
+//public class EditUserAction extends ActionSupport {
+//
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = -7641976286350960735L;
+//	
+//	/*array list holds info*/
+//	private List<Employee> employees = null;
+//	private List<Account> employeeAccount = null;
+//	
+//	/*declartions*/
+//	private String username;
+//	private String password;
+//	
+//	private String firstName;
+//	private String lastName;
+//	
+//
+//	
+//	private String email;
+//	
+//	public static final int MIN_PASSWORD_LENGTH = 8;
+//	public static final int MAX_PASSWORD_LENGTH = 35;
+//	
+//
+//	
+//	/*add and edit employee*/
+//	public String editUser()
+//	{
+//		System.out.println("hello");
+//		employees = EmpBO.fetchEmployees();
+//		
+//				for(int k = 0; k<employees.size(); k++)
+//				{
+//					System.out.println("checking for id");
+//					System.out.println(employees.get(k).getFirstName()+" "+ employees.get(k).getLastName() + "  "+ employees.get(k).getId());
+//				}
+//				
+//		
+//		return "SUCCESS";
+//	}
+//	
+//	
+//	
+//	/*getter and setter*/
+//	public String getUsername() {
+//		return username;
+//	}
+//	
+//	public String getPassword()
+//	{
+//		return password;
+//	}
+//	
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//	
+//	public void setPassword(String password)
+//	{
+//		this.password = password;
+//		
+//	}
+//	
+//	public List<Employee> getEmployees()
+//	{
+//		return employees;
+//	}
+//	
+//	public void setEmployees(List<Employee> employees)
+//	{
+//		this.employees = employees;
+//	}
+//	
+//	
+//	public String getFirstname()
+//	{
+//		return firstName;
+//	}
+//	
+//	public void setFirstname(String firstName) {
+//		this.firstName = firstName;
+//	
+//	}
+//	
+//	public String getLastname()
+//	{
+//		return lastName;
+//	}
+//	
+//	public void setLastname(String lastName) {
+//		this.lastName = lastName;
+//	
+//	}
+//	
+//	public String getEmail()
+//	{
+//		return email;
+//	}
+//	
+//	public void setEmail(String email) {
+//		this.email = email;
+//	
+//	}
+//	
+//	
+//	
+//	
+//}
--- strutsTemplate/src/main/webapp/empEdit.jsp
+++ strutsTemplate/src/main/webapp/empEdit.jsp
@@ -1,46 +1,46 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%-- <%@ page language="java" contentType="text/html; charset=ISO-8859-1" --%> --%>
+<!--     pageEncoding="ISO-8859-1"%> -->
      
-<%@ taglib prefix="s" uri="/struts-tags"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Employee Add/Edit Page</title>
-</head>
+<%-- <%@ taglib prefix="s" uri="/struts-tags"%> --%>
+<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> -->
+<!-- <html> -->
+<!-- <head> -->
+<!-- <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"> -->
+<!-- <title>Employee Add/Edit Page</title> -->
+<!-- </head> -->
 
-<h3>Welcome <s:property value="username"></s:property></h3>
-<body>
-<s:form action="saveEditEmp">
+<%-- <h3>Welcome <s:property value="username"></s:property></h3> --%>
+<!-- <body> -->
+<%-- <s:form action="saveEditEmp"> --%>
 
-<s:textfield name="firstName" label="First Name" />
-		<s:textfield name="lastName" label="Last Name" />
-		<s:textfield name="address" label="Address" />
-		<s:textfield name="city" label="City" />
-		 <s:select label= "State" headerValue ="Select"  name="state" list= "#{'':'Select','AL':'AL', 'AK':'AK', 'DE': 'DE',
-		 'DC': 'DC', 'FL': 'FL','GA':'GA', 'HI':'HI', 'ID': 'ID', 'IL': 'IL', 'IN': 'IN', 'IA':'IA', 'KS':'KS', 'KY': 'KY',
-		 'LA': 'LA', 'ME': 'ME','MD':'MD', 'MA':'MA', 'MI': 'MI', 'MN': 'MN', 'MS': 'MS', 'MO':'MO', 'MT':'MT', 'NE': 'NE',
-		 'NV': 'NV', 'NH': 'NH','NJ':'NJ', 'NM':'NM', 'NY': 'NY', 'NC': 'NC', 'ND': 'ND', 'OH':'OH', 'OK':'OK', 'OR': 'OR',
-		 'PA': 'PA', 'RI': 'RI','SC':'SC', 'SD':'SD', 'TN': 'TN', 'TX': 'TX', 'UT': 'UT', 'VT':'VT', 'VA':'VA', 'WA': 'WA',
-		 'WV': 'WV', 'WI': 'WI' }"/> 
+<%-- <s:textfield name="firstName" label="First Name" /> --%>
+<%-- 		<s:textfield name="lastName" label="Last Name" /> --%>
+<%-- 		<s:textfield name="address" label="Address" /> --%>
+<%-- 		<s:textfield name="city" label="City" /> --%>
+<%-- 		 <s:select label= "State" headerValue ="Select"  name="state" list= "#{'':'Select','AL':'AL', 'AK':'AK', 'DE': 'DE', --%>
+<%-- 		 'DC': 'DC', 'FL': 'FL','GA':'GA', 'HI':'HI', 'ID': 'ID', 'IL': 'IL', 'IN': 'IN', 'IA':'IA', 'KS':'KS', 'KY': 'KY', --%>
+<%-- 		 'LA': 'LA', 'ME': 'ME','MD':'MD', 'MA':'MA', 'MI': 'MI', 'MN': 'MN', 'MS': 'MS', 'MO':'MO', 'MT':'MT', 'NE': 'NE', --%>
+<%-- 		 'NV': 'NV', 'NH': 'NH','NJ':'NJ', 'NM':'NM', 'NY': 'NY', 'NC': 'NC', 'ND': 'ND', 'OH':'OH', 'OK':'OK', 'OR': 'OR', --%>
+<%-- 		 'PA': 'PA', 'RI': 'RI','SC':'SC', 'SD':'SD', 'TN': 'TN', 'TX': 'TX', 'UT': 'UT', 'VT':'VT', 'VA':'VA', 'WA': 'WA', --%>
+<%-- 		 'WV': 'WV', 'WI': 'WI' }"/>  --%>
 		
-		<s:textfield name="zip" label="Zip/Postal Code" />
-		<s:textfield name="cellPhone" label="Cell Phone" />
-		<s:textfield name="homePhone" label="Home Phone" />
-		<s:textfield name="email" label="Email" />
-		<s:textfield name="id" label ="ID"/>
+<%-- 		<s:textfield name="zip" label="Zip/Postal Code" /> --%>
+<%-- 		<s:textfield name="cellPhone" label="Cell Phone" /> --%>
+<%-- 		<s:textfield name="homePhone" label="Home Phone" /> --%>
+<%-- 		<s:textfield name="email" label="Email" /> --%>
+<%-- 		<s:textfield name="id" label ="ID"/> --%>
 		
 		
-		<s:if test="hasActionErrors()">
-   <div class="EmployeeEditedSave">
-      <s:actionerror/>
-   </div>
-</s:if>
+<%-- 		<s:if test="hasActionErrors()"> --%>
+<!--    <div class="EmployeeEditedSave"> -->
+<%--       <s:actionerror/> --%>
+<!--    </div> -->
+<%-- </s:if> --%>
 		
-		<input type="submit" value="Add/Update Employee" name="save/update" />
-		<input type="submit" value="Cancel" name="Cancel" />
+<!-- 		<input type="submit" value="Add/Update Employee" name="save/update" /> -->
+<!-- 		<input type="submit" value="Cancel" name="Cancel" /> -->
 		
-	</s:form>
+<%-- 	</s:form> --%>
 	
 
-</body>
+<!-- </body> -->
